21b46a1763d7376fc97ddc80a46754da2e6c6fae
Sandis Kaģis <s20kagisand@venta.lv>
2024-04-11 15:00:24 +0300

    Add: ProductService

---
+++ src/main/java/lv/venta/service/IFilterProductService.java
@@ -0,0 +1,17 @@
+package lv.venta.service;
+
+import java.util.ArrayList;
+
+import lv.venta.model.Product;
+
+public interface IFilterProductService {
+	
+	public abstract ArrayList<Product> filterByPriceLessThanThreshold(float threshold) throws Exception;
+	
+	public abstract ArrayList<Product> filterByQuantityLessThanThreshold(int threshold) throws Exception;
+	
+	public abstract ArrayList<Product> filterByTitleOrDescription(String text) throws Exception;
+	
+	public abstract float calculateProductTotalValue() throws Exception;
+
+}
