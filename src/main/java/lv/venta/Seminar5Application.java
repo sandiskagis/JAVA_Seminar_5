31686dd372ad5644148c44e87d56eeda119ba3eb
Sandis Kaģis <s20kagisand@venta.lv>
2024-04-24 21:54:48 +0300

    New: CommandRunner function for database testing

--- src/main/java/lv/venta/Seminar5Application.java
+++ src/main/java/lv/venta/Seminar5Application.java
@@ -1,13 +1,42 @@
 package lv.venta;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import lv.venta.model.Product;
+import lv.venta.repo.IProductRepo;
 
 @SpringBootApplication
 public class Seminar5Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Seminar5Application.class, args);
+	}
+	
+	
+
+	@Bean //sī funkcija stratēsies automātiski, pēc programmas palaišanas
+	public CommandLineRunner testModelLayer(IProductRepo productRepo) {
+		return new CommandLineRunner() {
+			
+			@Override
+			public void run(String... args) throws Exception {
+				Product p1 = new Product("Abols", "Zali", 0.99f, 10);
+				Product p2 = new Product("Arbuzs", "Roza", 3.99f, 15);
+				Product p3 = new Product("Vinogas", "Zali", 5.99f, 50);
+				productRepo.save(p1);
+				productRepo.save(p2);
+				productRepo.save(p3);
+				System.out.println("How Many:" + productRepo.count());//3
+				System.out.println("Get:" + productRepo.findById(2).get());//Arbuzam
+				Product updatedPr = productRepo.findById(2).get();//Arbuzs
+				updatedPr.setQuantity(20);
+				productRepo.save(updatedPr);//šeit jau būs 20 Arbuzam
+				
+			}
+		};
 	}
 
 }
--- src/main/java/lv/venta/model/Product.java
+++ src/main/java/lv/venta/model/Product.java
@@ -1,5 +1,12 @@
 package lv.venta.model;
 
+import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
@@ -17,40 +24,42 @@ import lombok.ToString;
 @ToString
 @NoArgsConstructor
 //@AllArgsConstructor
+@Table(name = "ProductTable")
+@Entity
 public class Product {
 	
 	@Setter(value = AccessLevel.NONE)//priekš ID nebūs automātiskais set
+	@Column(name = "Id")
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
 	
 	@NotNull
 	@Size(min = 3, max = 20)
 	@Pattern(regexp = "[A-Z]{1}[a-z ]+", message = "Only letters allowed")
+	@Column(name = "Title")
 	private String title;
 	
 	@NotNull
 	@Size(min = 4, max = 200)
 	@Pattern(regexp = "[A-Za-z .:!]+")
+	@Column(name = "Description")
 	private String description;
 	
 	@Min(0)
 	@Max(10000)
+	@Column(name="Price")
 	private float price;
 	
 	@Min(0)
 	@Max(100)
+	@Column(name="Quantity")
 	private int quantity;
 	
-	
-	private static int counter = 1;
-	
-	public void setId() {
-		this.id = counter++;
-	}
 
 	public Product(@NotNull @Size(min = 3, max = 20) @Pattern(regexp = "[A-Z]{1}[a-z ]+") String title,
 			@NotNull @Size(min = 4, max = 200) @Pattern(regexp = "[A-Za-z .:!]+") String description,
 			@Min(0) @Max(10000) float price, @Min(0) @Max(100) int quantity) {
-		setId();
 		this.title = title;
 		this.description = description;
 		this.price = price;
