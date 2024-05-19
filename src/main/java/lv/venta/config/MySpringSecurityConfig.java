ca90c3f0e3040c9d8c28de778ad9931de896f64f
Sandis Kaģis <s20kagisand@venta.lv>
2024-05-19 14:49:02 +0300

    Update: change web config to users from DB

--- src/main/java/lv/venta/config/MySpringSecurityConfig.java
+++ src/main/java/lv/venta/config/MySpringSecurityConfig.java
@@ -2,6 +2,7 @@ package lv.venta.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -13,34 +14,39 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+import lv.venta.service.impl.MyUserDetailsManagerServiceImpl;
+
+//TODO uztaisīt divas modeļu kalses MyUser un MyAuthority 
+//.izveidot abiem repositopriju
+//Comandline runner funkcijā izveidot 2 lietotajums un piesiatīt 2 lomas
+
+
+
 @Configuration
 @EnableWebSecurity
 public class MySpringSecurityConfig {
 	
+
 	@Bean
-	public UserDetailsManager createTestUser() {
+	public MyUserDetailsManagerServiceImpl getService()
+	{
+		return new MyUserDetailsManagerServiceImpl();
+	}
+	
+	
+	
+	
+	@Bean
+	public DaoAuthenticationProvider createAuthProvider() {
 		PasswordEncoder encoder =
 			    PasswordEncoderFactories.createDelegatingPasswordEncoder();
-		
-		UserDetails u1Details = 
-				User
-				.builder()
-				.username("admin")
-				.password(encoder.encode("admin"))
-				.authorities("ADMIN")
-				.build();
-		
-		UserDetails u2Details = 
-				User
-				.builder()
-				.username("karina")
-				.password(encoder.encode("123"))
-				.authorities("USER")
-				.build();
-		
-		return new InMemoryUserDetailsManager(u1Details, u2Details);
-		
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+		provider.setPasswordEncoder(encoder);
+		provider.setUserDetailsService(getService());
+		return provider;
+	
 	}
+	
 	
 	@Bean
 	public SecurityFilterChain configureEndpoints(HttpSecurity http) throws Exception {
@@ -56,12 +62,13 @@ public class MySpringSecurityConfig {
 				.requestMatchers("/product/update**").hasAuthority("ADMIN")
 				.requestMatchers("/product/delete/**").hasAuthority("ADMIN")
 				.requestMatchers("/product/filter/**").hasAnyAuthority("USER", "ADMIN")
+				.requestMatchers("/h2-console/**").hasAuthority("ADMIN")
 				);//.exceptionHandling(exp->exp.accessDeniedPage("/access-denied"));
 		
 		//http.exceptionHandling(exp->exp.accessDeniedPage("/access-denied"));//nosaka, uz kuru url adresi japārlec
 		http.csrf(csrf->csrf.disable());
 		http.headers(frame->frame.frameOptions(option->option.disable()));
-				
+		
 		http.formLogin(form -> form.permitAll());
 		
 		
